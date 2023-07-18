07f46cd0e332bab23d0e5540243ef47c5934a578
Qinsi ZHU <qszhu2006@gmail.com>
2023-07-18 17:02:57 +0800

    auth interface

--- yanimws.nimble
+++ yanimws.nimble
@@ -1,4 +1,4 @@
-version     = "0.2.1"
+version     = "0.2.2"
 author      = "Qinsi ZHU"
 description = "Yet Another Nim Web Server"
 license     = "MIT"
---
+++ yanimws/baseAuth.nim
@@ -0,0 +1,13 @@
+import ./server
+
+
+
+type
+  BaseAuth* = ref object of RootObj
+    key*, secret*: string
+
+method genSign*(self: BaseAuth, c: YaContext): string {.base.} = discard
+
+method checkSign*(self: BaseAuth, c: YaContext): bool {.base.} = discard
+
+method signParams*(self: BaseAuth, params: YaRequestKV): YaRequestKV {.base.} = discard
