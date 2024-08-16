47866cf730d0581b04c417db0d52347fd043a645
Qinsi ZHU <qszhu2006@gmail.com>
2024-08-16 15:51:00 +0800

    adjust cors

--- yanimws/middleware/cors.nim
+++ yanimws/middleware/cors.nim
@@ -2,6 +2,9 @@ import ../server
 
 proc Cors*(allowOrigin = "*", allowHeaders = "*"): YaHandler =
   return proc(c: YaContext) {.async, gcsafe.} =
-    waitFor c.next()
+    if c.request.httpMethod == HttpOptions:
+      c.text ""
+    else:
+      waitFor c.next()
     c.response.headers["Access-Control-Allow-Origin"] = allowOrigin
     c.response.headers["Access-Control-Allow-Headers"] = allowHeaders
